0f39700cc66106230e46ef5b731f7504db4828b1
patel-heet <heetp420@gmail.com>
2025-05-24 23:36:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def predict():
     return jsonify({
         "spam": bool(prediction[0])  # Convert numpy int to bool
     })
-if name == "main":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
